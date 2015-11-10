d8613d7eb88e6442192b1e1db5a594d84194db5b
jasonianmurray <murray.jason.ian@gmail.com>
2015-11-10 15:05:02 +0000

    Updated version to include basic GUI implementation.
    The GUI is image button based (images still pending).

---
+++ app/src/main/java/team10/studentwellbeingapp/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package team10.studentwellbeingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_activity);
+    }
+}
