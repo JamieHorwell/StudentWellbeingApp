2f5df3faaf836cc0136f4e212463d0892c076f75
jasonianmurray <murray.jason.ian@gmail.com>
2015-11-27 19:50:09 +0000

    Locking all of the changes that Jamie and I have made together!
    ...and it actually worked!!!

--- app/src/main/java/team10/studentwellbeingapp/MainMenuActivity.java
+++ app/src/main/java/team10/studentwellbeingapp/MainMenuActivity.java
@@ -1,20 +1,35 @@
 /*
-Title:
 Initial Author: Jason Ian Murray, Date: 8/11/2015
 Notes:
-This is the activity that launches the main menu and at time of writing acts as the driver
-class.
+    -   This is the activity that launches the main menu and at time
+    of writing acts as the driver class.
 
 Extended By: Jason Ian Murray, Date: 10/11/15
 Notes:
-Extended to ad the button listeners for ImgButtons on
-main_menu_activity.xml
+    -   Extended to add the button listeners for ImgButtons on
+    main_menu_activity.xml
+
+Extended by: Jason Ian Murray, Date: 25/11/15
+Notes:
+    -   Replaced the somewhat clumsy button listeners with a simple
+    onButtonClick() method. This means that all buttons can trigger this
+    method and then be sorted by getId() for specific functionality. This
+    seemed to be a more elegant, less buggy solution than the original.
+    -   Prepared the switch statement to offer conditions based on
+    button id.
+
+Extended By: Jason Ian Murray, Date: 26/11/2015
+Notes:
+    -   Removed previous incarnation of toolbar code.
+    -   Added the toolbar; changed the previous implementation
+    and replaced it with a more stable XML variant.
 */
 
 package team10.studentwellbeingapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -24,16 +39,38 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.menuToolbar);
+        setSupportActionBar(toolbar);
     }
 
-    protected void menuButtonOnClick(View view){
-
+    public void onButtonClick(View v) {
+        ImageButton button = (ImageButton) v;
+        switch (button.getId()) {
+            case R.id.menuButton1:
+                startActivity(new Intent(this, ButtonOneActivity.class));
+                break;
+            case R.id.menuButton2:
+                startActivity(new Intent(this, ButtonTwoActivity.class));
+                break;
+            case R.id.menuButton3:
+                startActivity(new Intent(this, ButtonThreeActivity.class));
+                break;
+            case R.id.menuButton4:
+                startActivity(new Intent(this, ButtonFourActivity.class));
+                break;
+            case R.id.bookAppointmentButton:
+                startActivity(new Intent(this, BookingAppointmentActivity.class));
+                break;
+            case R.id.menuButton6:
+                startActivity(new Intent(this, ButtonSixActivity.class));
+                break;
+            case R.id.menuButton7:
+                startActivity(new Intent(this, ButtonSevenActivity.class));
+                break;
+            case R.id.menuButton8:
+                startActivity(new Intent(this, ButtonEightActivity.class));
+                break;
+        }
     }
-    /** Called when the user clicks the Book appintment button */
-    public void openBookingScreen(View view){
-       Intent intent = new Intent(this, BookingAppointmentActivity.class);
-        startActivity(intent);
-    }
-
-
 }
