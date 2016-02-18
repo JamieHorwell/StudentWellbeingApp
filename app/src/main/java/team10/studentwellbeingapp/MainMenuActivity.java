d6167698180d1d639d1d6e0f10677e721ff5c386
JamieHorwell <jamie.horwell995@gmail.com>
2016-02-18 13:52:10 +0000

    Implement listener, update manifest

--- app/src/main/java/team10/studentwellbeingapp/MainMenuActivity.java
+++ app/src/main/java/team10/studentwellbeingapp/MainMenuActivity.java
@@ -30,6 +30,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -42,6 +43,24 @@ public class MainMenuActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.menuToolbar);
         setSupportActionBar(toolbar);
+
+
+        String PROJECT_NUMBER = "AIzaSyCHK6ZRDYtCWGiZrbQGBK4lrz0QmNAEAMY";
+
+        GCMClientManager pushClientManager = new GCMClientManager(this, PROJECT_NUMBER);
+        pushClientManager.registerIfNeeded(new GCMClientManager.RegistrationCompletedHandler() {
+            @Override
+            public void onSuccess(String registrationId, boolean isNewRegistration) {
+
+                Log.d("Registration id", registrationId);
+                //send this registrationId to your server
+            }
+            @Override
+            public void onFailure(String ex) {
+                super.onFailure(ex);
+            }
+        });
+
     }
 
     public void onButtonClick(View v) {
---
+++ app/src/main/java/team10/studentwellbeingapp/MyInstanceIDListenerService.java
@@ -0,0 +1,7 @@
+package team10.studentwellbeingapp;
+
+/**
+ * Created by Jamie on 17/02/2016.
+ */
+public class MyInstanceIDListenerService {
+}
