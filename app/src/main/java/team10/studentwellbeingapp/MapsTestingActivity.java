a0e22551bb681e448fdc9acce781f1076d4f3ead
JamieHorwell <jamie.horwell995@gmail.com>
2016-04-12 00:08:25 +0100

    Maps now find counselling near users location!!!

--- app/src/main/java/team10/studentwellbeingapp/MapsTestingActivity.java
+++ app/src/main/java/team10/studentwellbeingapp/MapsTestingActivity.java
@@ -2,9 +2,16 @@ package team10.studentwellbeingapp;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -19,6 +26,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,18 +41,34 @@ public class MapsTestingActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Marker[] markers;
+    private final int MaxMarkers = 15;
+    String placesSearchStr;
+    private MarkerOptions[] places;
+    private Double usersLat;
+    private Double usersLng;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_testing);
-        setUpMapIfNeeded();
+        markers = new Marker[MaxMarkers];
 
+
+       
+        //placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&name=cruise&key=AIzaSyCUHT_xAneCI6EG7nGqsqQt_AbDPNV7Lzk";
+
+        setUpMapIfNeeded();
+        testMarker(testGeocodeMatches());
+        placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/" +
+                "json?key=AIzaSyCUHT_xAneCI6EG7nGqsqQt_AbDPNV7Lzk&location="+usersLat.toString()+","+usersLng.toString()+
+                "&radius=3000" +
+                "&keyword=counselling";
         if (mMap != null) {
             mMap.setMyLocationEnabled(true);
         }
-        testMarker(testGeocodeMatches());
+        new getPlaces().execute(placesSearchStr);
+
     }
 
     @Override
@@ -95,6 +121,9 @@ public class MapsTestingActivity extends FragmentActivity {
             }
 
 
+
+
+
         private double[] testGeocodeMatches() {
 
             List<Address> geocodeMatches = null;
@@ -109,6 +138,8 @@ public class MapsTestingActivity extends FragmentActivity {
             }
 
             if (!geocodeMatches.isEmpty()) {
+                usersLat = geocodeMatches.get(0).getLatitude();
+                usersLng = geocodeMatches.get(0).getLongitude();
                 return new double[] {geocodeMatches.get(0).getLatitude(), geocodeMatches.get(0).getLongitude()};
             }
             else {
@@ -124,4 +155,101 @@ public class MapsTestingActivity extends FragmentActivity {
     private void setUpMap() {
 
     }
+
+    class getPlaces extends AsyncTask<String, Void, String> {
+        JSONArray jsonArray = null;
+
+        protected String doInBackground(String... placesURL) {
+            StringBuilder placesBuilder = new StringBuilder();
+
+
+            for(String placeSearhURL: placesURL) {
+                try {
+                    URL url = new URL(placeSearhURL);
+
+
+                try {
+                    //open connection
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                    StringBuilder respondeStrBuilder = new StringBuilder();
+
+                    String inputStr;
+                    while ((inputStr = streamReader.readLine()) != null) {
+                       Log.w("mapdebugging", inputStr);
+                        respondeStrBuilder.append(inputStr);
+                    }
+                    placesBuilder = respondeStrBuilder;
+
+                }
+                catch (java.io.IOException e) {
+
+                }
+                }
+                catch (MalformedURLException e) {}
+            }
+            return placesBuilder.toString();
+
+        }
+
+        protected void onPostExecute(String result) {
+             if(markers!=null) {
+                 for(int pm=0; pm<markers.length; pm++) {
+                     if(markers[pm] != null) {
+                         markers[pm].remove();
+                     }
+                 }
+             }
+
+            try {
+                    JSONObject resultObject = new JSONObject(result);
+                    JSONArray placesArray = resultObject.getJSONArray("results");
+                    places = new MarkerOptions[placesArray.length()];
+                    for(int i = 0; i <placesArray.length(); i++) {
+                       //parse each place
+                        boolean missingValue = false;
+                        LatLng placeLL = null;
+                        String placeName="";
+                        String vicinity="";
+                        try {
+                            JSONObject placeObject = placesArray.getJSONObject(i);
+                            JSONObject loc = placeObject.getJSONObject("geometry").getJSONObject("location");
+                            placeLL = new LatLng(Double.valueOf(loc.getString("lat")),Double.valueOf(loc.getString("lng")));
+                            vicinity = placeObject.getString("vicinity");
+                            placeName = placeObject.getString("name");
+                        } catch(JSONException e) {
+                            missingValue = true;
+                        }
+                        if (missingValue) {
+                            places[i] = null;
+                        }
+                        else {
+                            places[i]=new MarkerOptions()
+                            .position(placeLL).title(placeName).snippet(vicinity);
+                        }
+                    }
+
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if(places!=null && markers!=null) {
+                    for(int i = 0; i<places.length && i<markers.length; i++ ) {
+
+                        if(places[i]!=null) {
+                            markers[i]= mMap.addMarker(places[i]);
+                        }
+                    }
+                }
+
+
+        }
+    }
+
+
+
+
 }
