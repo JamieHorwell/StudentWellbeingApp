03ba713a5c4b265a92fca718761894220b064319
LeoRickayzen <l.rickayzen1@ncl.ac.uk>
2016-04-21 16:41:01 +0100

    Added files via upload

--- app/src/main/java/team10/studentwellbeingapp/ButtonThreeActivity.java
+++ app/src/main/java/team10/studentwellbeingapp/ButtonThreeActivity.java
@@ -10,18 +10,41 @@ Notes:
 
 package team10.studentwellbeingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.TweetUi;
+import com.twitter.sdk.android.tweetui.UserTimeline;
+
+import io.fabric.sdk.android.Fabric;
 
 public class ButtonThreeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig =  new TwitterAuthConfig("xnhza9YzLRHzkFASRWlceJx8I", "C75gDnXLWlM2P57h7RhUeU9wBM9x5SuDfNJ7qA40axpRIVfIYK");
+        Fabric.with(this, new TwitterCore(authConfig), new TweetUi());
+
         setContentView(R.layout.button_three);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.buttonThreeToolbar);
         setSupportActionBar(toolbar);
+
+        UserTimeline userTimeline = new UserTimeline.Builder().screenName("NUSUMindTheGap").build();
+
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(userTimeline)
+                .build();
+
+        ListView listview = (ListView) findViewById(R.id.listView2);
+
+        listview.setAdapter(adapter);
     }
 }
